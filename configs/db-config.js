3a10277a5ae513a8fc5bb4ec5abb003225d53a83
Social Codia <socialcodia@gmail.com>
2022-02-08 05:28:47 +0530

    Code Updated

--- configs/db-config.js
+++ configs/db-config.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const dbConnection = () => {
-    const dbUrl = "mongodb://localhost:27017/farooquiherbal";
+    const dbUrl = process.env.MONGODB_URI;
     mongoose.connect(dbUrl)
         .then(() => console.log('Database Connection Success'))
         .catch((err) => console.log('Connection With Databse Failed. Reason ' + err.message));
