d5859ce926b5f49ff42998961cc2dc2282308497
Social Codia <socialcodia@gmail.com>
2022-02-08 05:43:48 +0530

    Code Updated

--- middlewares/error-middleware.js
+++ middlewares/error-middleware.js
@@ -1,6 +1,5 @@
 module.exports = (err, req, res, next) => {
     err.statusCode = err.statusCode || 500;
     err.message = err.message || 'Oops..! Something went wrong.';
-    console.log(err.stack);
     res.status(err.statusCode).json({ success: false, message: err.message });
 }
