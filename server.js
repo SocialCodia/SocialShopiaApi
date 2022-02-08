6e3d75b8a368ca4107a3b24cf76fa9311ed09b07
Social Codia <socialcodia@gmail.com>
2022-02-08 07:18:57 +0530

    Whitelisted all the domains

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const app = express();
 
 const corsOption = {
     credentials: true,
-    origin: ['http://localhost:3000', 'http://localhost:80']
+    origin: ['http://localhost:3000', 'http://localhost:80', '*']
 }
 
 app.use(cors(corsOption));
